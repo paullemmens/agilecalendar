e64e3cb16c2f312120e19ea21fca51466a260267
Paul Lemmens <paul.lemmens@gmail.com>
2021-09-24 16:43:58 +0200

    Add first content

---
+++ R/parse_calendar.R
@@ -0,0 +1,29 @@
+## Generate a data frame with a row for each week and variables for
+## calendar week number, agile week number, date of the monday of each week,
+## iteration name, increment label.
+
+#' @title Generate an Agile Calendar
+#'
+#' @description Based on the `configuration` item in the YAML configuration
+#'    this function generates a dataframe that can be used to visualize
+#'    a calendar with default and additional markers from the remainder
+#'    of the configuration file.
+#'
+#' @param cfg Configuration element from a YAML project configuration.
+#'
+#' @return A data frame / tibble with a row for each week and variables
+#'    useful for visualizing the calendar.
+#'
+#' @importFrom dplyr "%>%"
+#'
+#' @export
+generate_calendar <- function(cfg) {
+
+  res <- tibble::tibble(agile_wk = 1:52) %>%
+    dplyr::mutate(dstamp = lubridate::ymd(cfg$year_start) + lubridate::weeks(0:51),
+                  calendar_wk = lubridate::isoweek(dstamp),
+                  increment = gl(52 / cfg$increment_length, cfg$increment_length,
+                                 labels = paste0('PI ', cfg$year, '.', 1:4)))
+
+  return(res)
+}
