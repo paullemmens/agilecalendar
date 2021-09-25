94393e0d13056dffe5375d225882993b45184fe6
Paul Lemmens <paul.lemmens@gmail.com>
2021-09-25 23:26:17 +0200

    Draft calculations for iterations

--- R/parse_calendar.R
+++ R/parse_calendar.R
@@ -20,11 +20,18 @@
 generate_calendar <- function(cfg) {
 
   n_increments <- 52 / increment_length
+  iteration_repeats <- (cfg$increment_length - cfg$ip_length) / cfg$iteration_length
+
   res <- tibble::tibble(agile_wk = 1:52) %>%
     dplyr::mutate(dstamp = lubridate::ymd(cfg$year_start) + lubridate::weeks(0:51),
                   calendar_wk = lubridate::isoweek(dstamp),
                   increment = gl(n_increments, cfg$increment_length,
                                  labels = paste0('PI ', cfg$year, '.', 1:4)),
+                  ## FIXME: concept; labeling doesn't work
+                  iteration = factor(c(gl(iteration_repeats, cfg$iteration_length),
+                                       iteration_repeats + 1),
+                                     labels = paste0('PI ', cfg$year, '.' 1:4,
+                                                     '.' 1:iteration_repeats)))
 
   return(res)
 }
