2372975b7e9c6b6b6b49b93619921898a77702f3
Paul Lemmens <paul.lemmens@gmail.com>
2021-09-26 21:42:59 +0200

    Refactor and finish iterations and increments

--- R/parse_calendar.R
+++ R/parse_calendar.R
@@ -1,7 +1,3 @@
-## Generate a data frame with a row for each week and variables for
-## calendar week number, agile week number, date of the monday of each week,
-## iteration name, increment label.
-
 #' @title Generate an Agile Calendar
 #'
 #' @description Based on the `configuration` item in the YAML configuration
@@ -19,19 +15,23 @@
 #' @export
 generate_calendar <- function(cfg) {
 
-  n_increments <- 52 / increment_length
-  iteration_repeats <- (cfg$increment_length - cfg$ip_length) / cfg$iteration_length
+  n_increments <- 52 / cfg$increment_length
+  n_iterations <- (cfg$increment_length - cfg$ip_length) / cfg$iteration_length
 
   res <- tibble::tibble(agile_wk = 1:52) %>%
     dplyr::mutate(dstamp = lubridate::ymd(cfg$year_start) + lubridate::weeks(0:51),
                   calendar_wk = lubridate::isoweek(dstamp),
-                  increment = gl(n_increments, cfg$increment_length,
-                                 labels = paste0('PI ', cfg$year, '.', 1:4)),
-                  ## FIXME: concept; labeling doesn't work
-                  iteration = factor(c(gl(iteration_repeats, cfg$iteration_length),
-                                       iteration_repeats + 1),
-                                     labels = paste0('PI ', cfg$year, '.' 1:4,
-                                                     '.' 1:iteration_repeats)))
+                  increment = rep_len(rep.int(seq_len(n_increments),
+                                              rep.int(cfg$increment_length,
+                                                      n_increments)),
+                                      52),
+                  iteration = rep(c(rep.int(seq_len(n_iterations),
+                                            rep.int(cfg$iteration_length,
+                                                    n_iterations)),
+                                    'ip'),
+                                  n_increments)) %>%
+    dplyr::mutate(increment = paste0('PI ', cfg$year, '.', increment),
+                  iteration = paste0(increment, '.', iteration))
 
   return(res)
 }
