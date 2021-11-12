ca16f8600bf5e434a8e0fcfedbec34fe28ab3ace
Paul Lemmens <paul.lemmens@gmail.com>
2021-11-12 16:28:44 +0100

    Remove old code to replace with working stuff

--- R/plot_calendar.R
+++ R/plot_calendar.R
@@ -5,49 +5,30 @@ plot_calendar <- function(cal) {
       ggplot2::geom_line(aes(group = 1), size = 6) +
       ggplot2::facet_wrap(~ increment, scales = 'free_x', ncol = 1) +
       ggplot2::scale_x_date(date_breaks = '1 week', date_labels = '%V') +
-      labs(x = 'Week number', y = '', colour = 'Iteration') +
-      ## coord_fixed(ratio = 4) +
-      theme_void() +
-      theme(legend.position = 'bottom') +
-      ## geom_vline(aes(xintercept = as.Date(cfg$markers[[2]]$`deadline for VSRR`)), color = 'black') +
-      ## geom_label_repel(aes(x = as.Date(cfg$markers[[2]]$`deadline for VSRR`),
-      ##                     y = 1,
-      ##                     label = names(cfg$markers[[2]])),
-      ##                 nudge_x = 0.15,
-      ##                 nudge_y = 1,
-      ##                 box.padding = 0.5,
-      ##                 segment.curvature = -0.1,
-      ##                 segment.ncp = 3,
-      ##                 segment.angle = 20,
-      ##                 max.overlaps = Inf) +
-      ## scale_y_continuous(expand = expansion(mult = 2.5)) +
-      NULL
+      ggplot2::labs(x = 'Week number', y = '', colour = 'Iteration') +
+      ggplot2::geom_vline(data = cal %>% filter(!is.na(cadence_markers)),
+                          mapping = ggplot2::aes(xintercept = dstamp),
+                          color = 'darkslateblue',
+                          size = 2.0) +
+      ggrepel::geom_label_repel(mapping           = aes(label = cadence_markers),
+                                colour            = 'darkslateblue',
+                                nudge_x           = 0.15,
+                                nudge_y           = 1,
+                                box.padding       = 0.5,
+                                segment.curvature = -0.1,
+                                segment.ncp       = 3,
+                                segment.angle     = 20,
+                                max.overlaps      = Inf) +
+      ggplot2::theme_minimal() +
+      ggplot2::theme(
+        legend.position  = 'bottom',
+        text             = ggplot2::element_text(size    = 18),
+        axis.title.x     = ggplot2::element_text(vjust   = -0.2),
+        plot.caption     = ggplot2::element_text(size    = 11 * 0.8),
+        strip.background = ggplot2::element_rect(colour =  NA, fill =  NA),
+        strip.text       = ggplot2::element_text(size   =  rel(1.1)),
+        panel.grid       = ggplot2::element_blank(),
+        axis.text.y      = ggplot2::element_blank(),
+        axis.ticks.y     = ggplot2::element_blank()
+      )
 }
-
-## d %>%
-##   ggplot(aes(x = increment_wk, colour = forcats::as_factor(iteration_no), y = 1)) +
-##   geom_line(aes(group = 1), size = 6) +
-##   facet_wrap(~ increment, ncol = 1) +
-##   scale_x_continuous(n.breaks = 13) +
-##   coord_fixed(ratio = 4) +
-##   theme_void()
-
-## d %>%
-##   ggplot(aes(x = dstamp, colour = forcats::as_factor(iteration_no), y = 1)) +
-##   geom_line(aes(group = 1), size = 6) +
-##   facet_wrap(~ increment, scales = 'free_x', ncol = 1) +
-##   scale_x_date(date_breaks = '1 week', date_labels = '%V') +
-##   ## theme_void() +
-##   geom_vline(aes(xintercept = as.Date(cfg$markers[[2]]$`deadline for VSRR`)), color = 'black') +
-##   geom_label_repel(aes(x = as.Date(cfg$markers[[2]]$`deadline for VSRR`),
-##                        y = 1,
-##                        label = names(cfg$markers[[2]])),
-##                    nudge_x = 0.15,
-##                    nudge_y = 1,
-##                    box.padding = 0.5,
-##                    segment.curvature = -0.1,
-##                    segment.ncp = 3,
-##                    segment.angle = 20,
-##                    max.overlaps = Inf) +
-##   ## scale_y_continuous(expand = expansion(mult = 2.5)) +
-##   NULL
