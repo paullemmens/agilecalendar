3eaaaeb15d3bfd3c4a035ede76c81f975b5b89fe
Paul Lemmens <paul.lemmens@gmail.com>
2021-12-06 18:09:54 +0100

    Enable converting text to a configuration

--- R/configuration.R
+++ R/configuration.R
@@ -24,13 +24,24 @@ parse_agile_events <- function(event_list) {
 #' @description Load the configuration file for the generator.
 #'
 #' @param cfgfile A path to a configuration file.
+#' @param cfg_content A string that contains the actual content of the
+#'    calendar configuration. This is mostly to support the dashboard.
+#'    Defaults to NULL and overrides `cfgfile` if both are specified
+#'    simultaneously.
 #'
 #' @importFrom dplyr "%>%"
 #'
 #' @export
-read_config <- function(cfgfile) {
+read_config <- function(cfgfile = NULL, cfg_content = NULL) {
 
-  cfg <- yaml::yaml.load_file(cfgfile)
+  if (is.null(cfgfile) && is.null(cfg_content)) stop('cfgfile and cfg_content cannot both be null')
+
+  if (!is.null(cfg_content)) {
+    cfg <- yaml::yaml.load(cfg_content)
+  } else {
+    cfg <- yaml::yaml.load_file(cfgfile)
+  }
+
   cfg$agile_events <- parse_agile_events(cfg$agile_events)
   cfg$markers <- parse_agile_events(cfg$markers)
 
