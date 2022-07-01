903ea262df444d996959c92db47b1b6e6f0e476f
Paul Lemmens <paul.lemmens@gmail.com>
2022-07-01 15:43:14 +0200

    Clean up calendar plot

--- R/plot_calendar.R
+++ R/plot_calendar.R
@@ -17,14 +17,14 @@ plot_calendar <- function(cal) {
     ggplot2::ggplot(ggplot2::aes(x = dstamp, colour = forcats::as_factor(iteration_no), y = 1)) +
       ggplot2::geom_line(ggplot2::aes(group = 1), size = 6) +
       ggplot2::facet_wrap(~ increment, scales = 'free_x', ncol = 1) +
-      ggplot2::labs(x = 'Week number', y = '', colour = 'Iteration') +
       ggplot2::scale_x_datetime(date_breaks = '1 week', date_labels = '%V') +
+      ggplot2::labs(x = '\nCalendar week number', y = '', colour = 'Iteration') +
       ggplot2::theme_minimal() +
       ggplot2::theme(
         legend.position  = 'bottom',
-        text             = ggplot2::element_text(size    = 18),
-        axis.title.x     = ggplot2::element_text(vjust   = -0.2),
-        plot.caption     = ggplot2::element_text(size    = 11 * 0.8),
+        text             = ggplot2::element_text(size   = 18),
+        axis.title.x     = ggplot2::element_text(vjust  = -0.2),
+        plot.caption     = ggplot2::element_text(size   = 11 * 0.8),
         strip.background = ggplot2::element_rect(colour =  NA, fill =  NA),
         strip.text       = ggplot2::element_text(size   =  ggplot2::rel(1.1)),
         panel.grid       = ggplot2::element_blank(),
